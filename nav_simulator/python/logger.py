47dc86a16be51c8805f8d3db0efb588fa6b7c5d0
Darius Gray <darius.levi.gray@gmail.com>
2026-05-26 23:42:02 -0600

    add python code to subscribe to the algo node publisher

---
+++ nav_simulator/python/logger.py
@@ -0,0 +1,22 @@
+import argparse
+import zmq
+import json
+
+cli = argparse.ArgumentParser(None)
+cli.add_argument("-s", "--sub_addr", default="tcp://localhost:8081")
+
+args = cli.parse_args(None)
+
+if args.sub_addr:
+    print(f"subscriber address = {args.sub_addr}\n")
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect(args.sub_addr)
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
+socket.subscribe("")
+
+while True:
+    msg = socket.recv_string()
+    data = json.loads(msg)
+    print(f"{data}")
